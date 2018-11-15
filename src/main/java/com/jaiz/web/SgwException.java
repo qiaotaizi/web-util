9a2f3aee95ac8d28e21272e4ecca339a3c9e0c81
jaiz <qiaotaiziqtz@gmail.com>
2018-11-15 22:36:32 +0800

    加入自定义异常

---
+++ src/main/java/com/jaiz/web/SgwException.java
@@ -0,0 +1,12 @@
+package com.jaiz.web;
+
+public class SgwException extends RuntimeException{
+
+    public SgwException(){
+        super();
+    }
+
+    public SgwException(String message){
+        super(message);
+    }
+}
